4b52f28c120d8a9e939f6fdfce354df5bf3a52aa
geekyasif <mdasif08737@gmail.com>
2023-08-09 22:13:58 +0530

    inital commit

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-(async () => {
+ const fetchData = async () => {
     const res = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-07-09&sortBy=publishedAt&apiKey=ac258abf3796495ca9d679cb402584fb")
-    const data = await res.json()
-    const articles = data.articles
+    const data = await res?.json()
+    const articles = data?.articles
     displayDataToHtml(articles)
-})()
+}
 
+fetchData()
 
 function displayDataToHtml(articles){
     
     addArticlesToSidebar(articles)
     // addArticlesToFirstArticleRow(articles.slice(0,3))
     // addArticlesToSecondArticleRow(articles.slice(3,6))
-    showArticles(articles.slice(0,3), "first-articles-row")
-    showArticles(articles.slice(3,6), "second-articles-row")
+    showArticles(articles?.slice(0,3), "first-articles-row")
+    showArticles(articles?.slice(3,6), "second-articles-row")
 }
 
 
@@ -22,9 +23,9 @@ function addArticlesToSidebar(articles) {
 
     const sidebarArticleContainer = document.getElementById("sidebar-articles-container")
     let _articles = ""
-    articles.forEach(article => {
-        _articles += `<h4>${article.title}</h4>
-                        <p class="date">${article.publishedAt}</p>
+    articles?.forEach(article => {
+        _articles += `<h4>${article?.title}</h4>
+                        <p class="date">${article?.publishedAt}</p>
                         `
     });
     sidebarArticleContainer.innerHTML = _articles
@@ -69,10 +70,10 @@ function showArticles(articles, id) {
 
     const container = document.getElementById(id)
     let _articles = ""
-    articles.forEach(article => {
+    articles?.forEach(article => {
         _articles += `<div class="article">
-        <h3 class="article-heading">${article.title}</h3>
-        <p class="article-description">${article.description}</p>
+        <h3 class="article-heading">${article?.title}</h3>
+        <p class="article-description">${article?.description}</p>
         </div> `
     });
 
